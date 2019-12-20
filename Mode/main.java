e77d3cd6929bdd178e447d0ac1fbd389e1c7fffa
Gracesta <50622355+Gracesta@users.noreply.github.com>
2019-12-20 14:28:40 +0900

    Update main.java

--- Mode/main.java
+++ Mode/main.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.TimerTask; 
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Arrays;
@@ -7,7 +8,7 @@ import java.util.Arrays;
 class Unit{                                                     //マップのセルのクラス
     public int x, y;
     public int status =  1;                                                 //statu = 0:障害物（移動不可）,　 statu = 1:空地（移動可能）, 　statu = 2:死亡エリア（爆弾の余波)
-    
+    //ImageIcon img;
     public void Unit(int x, int y){
       this.x = x; this.y = y;
     }
@@ -34,6 +35,7 @@ class Player extends Unit{
     int speed = 1;                  //　プレイヤーの初期速度
     int bomb_num = 3;               //　持っている爆弾の数
     int power = 2;                  //爆弾の最初の威力は2マス
+    //ImageIcon img;
     public int setBomb(){           //　爆弾を持っていれば、設置可能
         if(bomb_num > 0){
             bomb_num--;
@@ -51,47 +53,53 @@ class Player extends Unit{
     }
 }
 
-class Boomb extends TimerTask{          // class b = new Bomb(range);
-    //running timer task as daemon thread
-    Timer timer = new Timer(true);
-    int range;
-    public void Boomb( ){
-        this.range = 2;         //　爆弾の威力を設置(プレイヤーによる)
-        timer.schedule(this, 2000); //explodes in 2 seconds
+class Boomb extends TimerTask{
+  int x, y;
+  int range;
+  Unit[][] map;
+  java.util.Timer timer;
+  //ImageIcon img;
+  public Boomb(Unit[][] map, int x, int y){            // give coordinate when set a bomb
+    this.range = 2;         //　爆弾の威力を設置(プレイヤーによる)
+    this.map = map;
+    this.x = x; this.y = y;
+    timer = new java.util.Timer(true);
+    timer.schedule(this, 2000);
+  }
+
+  public void run() {                     // explode
+    //this.bomb_num++;                    // 　プレイヤーの爆弾の持つ数が１回復する
+    for(int i=1;i <= range;i++){          //　爆弾の爆発したところは２に塗り替える、プレイヤーがstatus＝２のところで立ったら死亡
+      map[this.x+i][this.y].setStatus(2);
+      map[this.x-i][this.y].setStatus(2);
+      map[this.x][this.y].setStatus(2);
+      map[this.x][this.y].setStatus(2);
     }
-    
-    private void explode(Unit Map[][]) {        // 爆発
-        //this.bomb_num++;                    // 　プレイヤーの爆弾の持つ数が１回復する
-        for(int i=1;i <= range;i++){         //　爆弾の爆発したところは２に塗り替える、プレイヤーがstatus＝２のところで立ったら死亡
-          Map[this.x+i][this.y].setStatus(2);
-          Map[this.x-i][this.y].setStatus(2);
-          Map[this.x][this.y].setStatus(2);
-          Map[this.x][this.y].setStatus(2);
-        }
-        Thread.sleep(500);                    //　爆弾の余波は0.5秒続く
-        for(int i=1;i <= range;i++){          //　爆弾の爆発が終わって、死亡エリア解除
-          Map[this.x+i][this.y].setStatus(1);  
-          Map[this.x-i][this.y].setStatus(1);
-          Map[this.x][this.y].setStatus(1);
-          Map[this.x][this.y].setStatus(1);
-        }
+    //need repaint ?
+    try {
+      Thread.sleep(500);                 //　爆弾の余波は0.5秒続く
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }                 
+    for(int i=1;i <= range;i++){          //　爆弾の爆発が終わって、死亡エリア解除
+      map[this.x+i][this.y].setStatus(1);  
+      map[this.x-i][this.y].setStatus(1);
+      map[this.x][this.y].setStatus(1);
+      map[this.x][this.y].setStatus(1);
     }
-    public void run() {
-      explode();
   }
 }
 
 class main {
-  //Timer timer = new Timer();
-  //timer.schedule(this, 2000);
   public static void main(String argv[]) {
-    Unit[][] Map = new Unit[100][100];
+    Unit[][] map = new Unit[100][100];
     for(int i=0;i<100;i++){
       for(int j=0;j<100;j++){
-        Map[i][j] = new Unit();
-        Map[i][j].setXY(i, j);
+        map[i][j] = new Unit();
+        map[i][j].setXY(i, j);
       }
     }
-    System.out.println(Map[50][70].getX());
+    Boomb boomb = new Boomb(map, 50, 70);
+    System.out.println(map[50][70].getX());
   }
 }
